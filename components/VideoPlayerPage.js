3c5fc42e57feddad2240e98e1d61d036482aaa3a
qzw881130 <qzw881130@gmail.com>
2024-09-22 10:17:18 +0800

    dev

--- components/VideoPlayerPage.js
+++ components/VideoPlayerPage.js
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
-import { View, StyleSheet, Dimensions, Image, TouchableOpacity, PanResponder } from 'react-native';
-import { Appbar, IconButton, Text } from 'react-native-paper';
+import { View, StyleSheet, Dimensions, Image, TouchableOpacity, PanResponder, TouchableWithoutFeedback } from 'react-native';
+import { Appbar, IconButton, Text, Drawer, List, Button, Divider, SegmentedButtons, RadioButton, Switch } from 'react-native-paper';
 import { SafeAreaView, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient';
@@ -10,6 +10,8 @@ const { width, height } = Dimensions.get('window');
 export const VideoPlayerPage = ({ video, onClose, onNextVideo }) => {
     const insets = useSafeAreaInsets();
     const [lastSwipeTime, setLastSwipeTime] = useState(0);
+    const [isSettingsVisible, setIsSettingsVisible] = useState(false);
+    const [activeSettings, setActiveSettings] = useState('');
 
     const panResponder = useRef(
         PanResponder.create({
@@ -34,6 +36,14 @@ export const VideoPlayerPage = ({ video, onClose, onNextVideo }) => {
         } else {
             console.log('onClose is not defined');
         }
+    };
+
+    const handleSettingsPress = () => {
+        setIsSettingsVisible(true);
+    };
+
+    const closeSettings = () => {
+        setIsSettingsVisible(false);
     };
 
     useEffect(() => {
@@ -65,9 +75,95 @@ export const VideoPlayerPage = ({ video, onClose, onNextVideo }) => {
                         <Appbar.BackAction color="white" />
                     </TouchableOpacity>
                     <Appbar.Content title="" />
-                    <Appbar.Action icon="cog" onPress={() => console.log('Settings pressed')} color="white" />
+                    <Appbar.Action icon="cog" onPress={handleSettingsPress} color="white" />
                 </Appbar.Header>
             </LinearGradient>
+            {isSettingsVisible && (
+                <TouchableWithoutFeedback onPress={closeSettings}>
+                    <View style={styles.overlay}>
+                        <TouchableWithoutFeedback>
+                            <View style={styles.drawerContainer}>
+                                <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', marginBottom: 10 }}>
+                                    <Text style={{ color: '#000', fontSize: 18 }}>控制面板</Text>
+                                    <TouchableOpacity onPress={() => setIsSettingsVisible(false)}>
+                                        <IconButton
+                                            icon="close"
+                                            size={24}
+                                            color="#000"
+                                            onPress={() => setIsSettingsVisible(false)}
+                                        />
+                                    </TouchableOpacity>
+                                </View>
+                                <Divider />
+                                <View style={styles.settingRow}>
+                                    <Text style={styles.settingLabel}>播放倍速</Text>
+                                    <View style={styles.settingControl}>
+                                        <SegmentedButtons
+                                            density="small"
+                                            buttons={[
+                                                { label: '0.5x', value: '0.5x' },
+                                                { label: '1x', value: '1x' },
+                                                { label: '1.5x', value: '1.5x' },
+                                                { label: '2x', value: '2x' },
+                                            ]}
+                                            onValueChange={(value) => console.log(`${value} pressed`)}
+                                        />
+                                    </View>
+                                </View>
+
+                                <View style={styles.settingRow}>
+                                    <Text style={styles.settingLabel}>播放模式</Text>
+                                    <View style={styles.settingControl}>
+                                        <RadioButton.Group
+                                            onValueChange={value => console.log(`${value} selected`)}
+                                            value="single"
+                                        >
+                                            <View style={styles.radioButtonGroup}>
+                                                <View style={styles.radioButtonRow}>
+                                                    <RadioButton value="single" />
+                                                    <Text style={styles.radioButtonLabel}>单视频循环</Text>
+                                                </View>
+                                                <View style={styles.radioButtonRow}>
+                                                    <RadioButton value="auto" />
+                                                    <Text style={styles.radioButtonLabel}>自动播放下一个</Text>
+                                                </View>
+                                            </View>
+                                        </RadioButton.Group>
+                                    </View>
+                                </View>
+
+                                <View style={styles.settingRow}>
+                                    <Text style={styles.settingLabel}>播放顺序</Text>
+                                    <View style={styles.settingControl}>
+                                        <RadioButton.Group
+                                            onValueChange={value => console.log(`${value} selected`)}
+                                            value="order"
+                                        >
+                                            <View style={styles.radioButtonGroup}>
+                                                <View style={styles.radioButtonRow}>
+                                                    <RadioButton value="order" />
+                                                    <Text style={styles.radioButtonLabel}>顺序播放</Text>
+                                                </View>
+                                                <View style={styles.radioButtonRow}>
+                                                    <RadioButton value="random" />
+                                                    <Text style={styles.radioButtonLabel}>随机播放</Text>
+                                                </View>
+                                            </View>
+                                        </RadioButton.Group>
+                                    </View>
+                                </View>
+
+                                <View style={styles.settingRow}>
+                                    <Text style={styles.settingLabel}>自动播放</Text>
+                                    <View style={styles.settingControl}>
+                                        <Switch value={true} onValueChange={(value) => console.log(`Auto play ${value}`)} />
+                                    </View>
+                                </View>
+                            </View>
+                        </TouchableWithoutFeedback>
+                    </View>
+                </TouchableWithoutFeedback>
+            )}
         </View>
     );
 };
@@ -114,5 +210,62 @@ const styles = StyleSheet.create({
     title: {
         color: 'white',
         fontSize: 18,
+    },
+    drawerContainer: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        // height: 200, // 调整抽屉的高度
+        backgroundColor: 'white',
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: -2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
+        padding: 10,
+    },
+    settingRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 10,
+    },
+    settingLabel: {
+        fontSize: 14,
+        color: '#333',
+        flex: 1, // 添加这一行
+    },
+    settingControl: {
+        flex: 5, // 增加这个值，给按钮组更多空间
+        alignItems: 'flex-start',
+    },
+    radioButtonGroup: {
+        flexDirection: 'row', // 使按钮组水平排列
+        justifyContent: 'flex-start', // 靠右对齐
+    },
+    radioButtonRow: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginLeft: 10, // 给每个选项之间添加一些间距
+    },
+    radioButtonLabel: {
+        marginLeft: 4,
+        fontSize: 12, // 减小字体大小以适应一行
+        color: '#333',
+    },
+    overlay: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        backgroundColor: 'rgba(0,0,0,0.5)',
+        justifyContent: 'flex-end',
     },
 });
