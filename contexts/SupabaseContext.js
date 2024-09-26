b60fd78b1c20365009e6000e8774799e2c01074c
qzw881130 <qzw881130@gmail.com>
2024-09-26 10:31:28 +0800

    dev

--- contexts/SupabaseContext.js
+++ contexts/SupabaseContext.js
@@ -1,6 +1,5 @@
 import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '@env';
 import { useSupabaseClient } from '../hooks/useSupabaseClient';
 
 const SupabaseContext = createContext(null);
@@ -9,12 +8,6 @@ export const SupabaseProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [session, setSession] = useState(null);
     const supabase = useSupabaseClient();
-    // const supabase = useMemo(() => createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
-    //     persistSession: true,
-    //     autoRefreshToken: true,
-    //     detectSessionInUrl: false,
-    // }), []);
-
     useEffect(() => {
         // 检查当前会话并设置用户
         const checkSession = async () => {
